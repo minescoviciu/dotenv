9a3f8d4e59e3126d10177d00893534be32431e19
Andrei Minescu <andreiminescu@gmail.com>
2023-11-03 07:17:29 +0200

    Init Commit

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -f "~/.config/nvim" ]; then
+    mkdir -p ~/.config/nvim
+fi
+
+CWD=$(pwd)
+ln -s $CWD/nvim/init.lua ~/.config/nvim/init.lua
+ln -s $CWD/tmux.conf ~/.tmux.conf
+ln -s $CWD/gitconfig ~/.gitconfig
+
+chekc_if_install () {
+    if [ -n "$(type $1)" ]; then
+        echo "$1 is installed"
+    else
+        echo -e "\033[31m$1 is NOT installed\033[0m"
+    fi
+}
+
+# BINARIES=("nvim", "git", "tmux")
+# for binary in ${BINARIES[@]}; do
+#     chekc_if_install $binary
+# done
+
