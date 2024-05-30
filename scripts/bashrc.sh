951363a70c95ef9a7fb2ca3bcb3748b6229507f3
Andrei Minescu <andreiminescu@gmail.com>
2024-05-30 22:44:43 +0300

    Improve ipython colors with catpuccino theme

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PROMPT_TOOLKIT_COLOR_DEPTH=DEPTH_24_BIT
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
