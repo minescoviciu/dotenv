2e156094ac7d70846748eee3d3248a6eb2372ba6
Andrei Minescu <aminescu@drivenets.com>
2024-10-25 14:41:25 +0000

    Copilot Chat Ask

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,8 +1,23 @@
+local my_ask = function()
+    local chat = require("CopilotChat")
+    local selection = require("CopilotChat.select").visual
+    local input = vim.fn.input("Ask Copilot: ")
+    if input == "" then
+        return
+    end
+    chat.ask(input, {
+        selection = selection,
+        filetype = vim.bo.filetype,
+        filename = vim.fn.expand("%:t"),
+    })
+end
+
 return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
         branch = "canary",
         dependencies = {
+            { "nvim-telescope/telescope.nvim"},
             { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
@@ -18,9 +33,14 @@ return {
             -- See Configuration section for rest
         },
         keys = {
-            {"<leader>ct", "<cmd>CopilotChatToggle<cr>", desc="[C]opilotChat [T]ogle"},
+            {"<leader>co", "<cmd>CopilotChatToggle<cr>", desc="[C]opilotChat T[O]ggle"},
             {"<leader>ce", "<cmd>CopilotChatExplain<cr>", desc="[C]opilotChat [E]xplain"},
             {"<leader>ce", "<cmd>CopilotChatExplain<cr>", desc="[C]opilotChat [E]xplain", mode="x"},
+            {"<leader>cr", "<cmd>CopilotChatReview<cr>", desc="[C]opilotChat [R]eview", mode="x"},
+            {"<leader>cd", "<cmd>CopilotChatFixDiagnostic<cr>", desc="[C]opilotChat Fix [D]iagnostic", mode="x"},
+            {"<leader>cl", "<cmd>CopilotChatReset<cr>",         desc = "[C]opilotChat C[l]ear buffer and chat history" },
+            {"<leader>cc", my_ask, desc = "[C]opilotChat [A]sk"},
+            {"<leader>cc", my_ask, desc = "[C]opilotChat [A]sk", mode="x"},
         },
         -- See Commands section for default commands if you want to lazy load on them
     },
