b9bc5b5a273069d1a6259ce73ebe936dcacbff70
Andrei Minescu <andreiminescu@gmail.com>
2025-02-10 08:14:40 +0200

    Replace telescope with snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -13,6 +13,47 @@ return {
     },
     bigfile = { enabled = true },
     dashboard = { enabled = true },
+    explorer = { 
+      enabled = true,
+    },
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+        },
+        explorer = {
+          hidden = true,
+          follow_file = true,
+          layout = {
+            preview = {
+              main = true,
+              enabled = false,
+            },
+            layout = {
+              position = "right"
+            }
+          },
+        },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<c-j>"] = {"list_scroll_down", mode = { "i", "n" }},
+            ["<c-k>"] = {"list_scroll_up", mode = { "i", "n" }},
+            ["<c-u>"] = {"preview_scroll_up", mode = { "i", "n" }},
+            ["<c-d>"] = {"preview_scroll_down", mode = { "i", "n" }},
+          },
+        },
+        list = {
+          keys = {
+            ["<c-j>"] = {"list_scroll_down", mode = { "i", "n" }},
+            ["<c-k>"] = {"list_scroll_up", mode = { "i", "n" }},
+            ["<c-u>"] = {"preview_scroll_up", mode = { "i", "n" }},
+            ["<c-d>"] = {"preview_scroll_down", mode = { "i", "n" }},
+          }
+        },
+      },
+    },
     indent = { enabled = true },
     input = { enabled = false, },
     notifier = {
@@ -27,9 +68,51 @@ return {
       notification = {
         -- wo = { wrap = true } -- Wrap notifications
       }
-    }
+    },
   },
   keys = {
+    { "<leader><leader>", function() Snacks.picker.resume() end, desc = "Resume" },
+    -- find
+    { "<leader>sf", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>sb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fb", function() Snacks.picker.explorer() end, desc = "Explorer" },
+    -- { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    -- git
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gF", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    -- Grep
+    { "<leader>sB", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sG", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+    { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    -- search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+    { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
+    { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+    { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+    { "<leader>sD", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+    { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    -- LSP
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
     { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
