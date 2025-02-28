42a87320aef377e58744375c99d0e128881f7b36
Andrei Minescu <andreiminescu@gmail.com>
2025-02-28 08:37:09 +0200

    Change Copilot-chat with codecompanion

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,56 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    keys = {
+        { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n" }, desc = "Toggle CodeCompanion Chat" },
+        { "<leader>aa", "<cmd>CodeCompanionChat<cr>",        mode = { "v" }, desc = "Toggle CodeCompanion Chat" },
+    },
+    config = function(_, opts)
+        require("codecompanion").setup({
+            adapters = {
+                copilot = require("codecompanion.adapters").extend("copilot", {
+                    schema = {
+                        model = {
+                            default = "claude-3.7-sonnet",
+                        },
+                    },
+                }),
+            },
+            strategies = {
+                chat = {
+                    adapter = "copilot",
+                },
+                inline = {
+                    adapter = "copilot",
+                },
+                agent = {
+                    adapter = "copilot",
+                },
+            },
+            chat = {
+                slash_commands = {
+                    ["file"] = {
+                        -- Location to the slash command in CodeCompanion
+                        callback = "strategies.chat.slash_commands.file",
+                        description = "Select a file using Snacks",
+                        opts = {
+                            provider = "snacks", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+                            contains_code = true,
+                        },
+                    },
+                    ['buffer'] = {
+                        opts = {
+                            provider = 'snacks',
+                        },
+                    },
+                },
+            },
+
+        }
+        )
+        vim.cmd([[cab cc CodeCompanion]])
+    end,
+}
