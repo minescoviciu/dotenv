694e433fc32085c3fe38481186064fc64a6f301c
Andrei Minescu <andreiminescu@gmail.com>
2025-03-01 22:57:20 +0200

    Revert to claude 3.5 as 3.7 doesn't work

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -14,7 +14,7 @@ return {
                 copilot = require("codecompanion.adapters").extend("copilot", {
                     schema = {
                         model = {
-                            default = "claude-3.7-sonnet",
+                            default = "claude-3.5-sonnet",
                         },
                     },
                 }),
