723e77392413265fde9f786646fd6f5fe7abf91a
Andrei Minescu <andreiminescu@gmail.com>
2025-03-08 16:42:06 +0200

    Add copilot completion without blink

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -18,11 +18,11 @@ return {
                 },
             },
             suggestion = {
-                enabled = false,
+                enabled = true,
                 auto_trigger = true,
                 debounce = 75,
                 keymap = {
-                    accept = "<M-l>",
+                    accept = "<tab>",
                     accept_word = false,
                     accept_line = false,
                     next = "<M-]>",
