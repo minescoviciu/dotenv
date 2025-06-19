141aecaa89684e357a558c3b4793910f210400a2
Andrei <andreiminescu@gmail.com>
2025-06-19 16:09:11 +0000

    Make input relative to cursor

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -76,7 +76,12 @@ return {
       },
     },
     indent = { enabled = true },
-    input = { enabled = true, },
+    input = { 
+      enabled = true,
+      win = {
+        style = 'above_cursor',
+      }, 
+    },
     notifier = {
       enabled = true,
       timeout = 3000,
@@ -88,7 +93,39 @@ return {
     styles = {
       notification = {
         -- wo = { wrap = true } -- Wrap notifications
-      }
+      },
+      above_cursor = {
+        backdrop = false,
+        position = 'float',
+        border = 'rounded',
+        title_pos = 'left',
+        height = 1,
+        noautocmd = true,
+        relative = 'cursor',
+        row = -3,
+        col = 0,
+        wo = {
+          cursorline = false,
+        },
+        bo = {
+          filetype = 'snacks_input',
+          buftype = 'prompt',
+        },
+        --- buffer local variables
+        b = {
+          completion = true, -- enable/disable blink completions in input
+        },
+        keys = {
+          n_esc = { '<esc>', { 'cmp_close', 'cancel' }, mode = 'n', expr = true },
+          i_esc = { '<esc>', { 'cmp_close', 'stopinsert' }, mode = 'i', expr = true },
+          i_cr = { '<cr>', { 'cmp_accept', 'confirm' }, mode = 'i', expr = true },
+          i_tab = { '<tab>', { 'cmp_select_next', 'cmp', 'fallback' }, mode = 'i', expr = true },
+          i_ctrl_w = { '<c-w>', '<c-s-w>', mode = 'i', expr = true },
+          i_up = { '<up>', { 'hist_up' }, mode = { 'i', 'n' } },
+          i_down = { '<down>', { 'hist_down' }, mode = { 'i', 'n' } },
+          q = "cancel",
+        },
+      },
     },
   },
   keys = {
